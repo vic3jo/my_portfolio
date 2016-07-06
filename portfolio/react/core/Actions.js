8b0c1b9c960ba60c26bca199d1cf0c5cdf8fa217
Victor <vet6013@rit.edu>
2016-07-06 13:32:47 -0400

    Refactoring actions

--- portfolio/react/core/Actions.js
+++ portfolio/react/core/Actions.js
@@ -1,58 +1,65 @@
 import * as ActionTypes from './ActionTypes';
+import * as URLS from './URLS';
 import axios from 'axios';
-
-export const LOAD_EDUCATION_HISTORY_DATA_URL = 'http://localhost:8000/education-history/';
-export const LOAD_ACADEMIC_PROJECTS_DATA_URL = 'http://localhost:8000/academic-projects/';
-export const LOAD_PERSONAL_PROJECTS_DATA_URL = 'http://localhost:8000/personal-projects/';
-export const LOAD_ABOUT_DATA_URL = 'http://localhost:8000/about/';
 
 
 const loadEducationData = (store) => {
-	axios.get(LOAD_EDUCATION_HISTORY_DATA_URL).then((response)=>{
+	axios.get(URLS.LOAD_EDUCATION_HISTORY_DATA).then((response)=>{
 		store.dispatch({
 			type: ActionTypes.LOAD_EDUCATION_HISTORY_DATA,
 			payload: response.data
 		});
 	}).catch((response) => {
-	    console.log(response);
+	    store.dispatch({
+			type: ActionTypes.ERROR_LOADING_DATA,
+			payload: response
+		});
 	});
 	
 };
 
 
 const loadAcademicProjectsData = (store) => {
-	axios.get(LOAD_ACADEMIC_PROJECTS_DATA_URL).then((response)=>{
+	axios.get(URLS.LOAD_ACADEMIC_PROJECTS_DATA).then((response)=>{
 		store.dispatch({
 			type: ActionTypes.LOAD_ACADEMIC_PROJECTS_DATA,
 			payload: response.data
 		});
 	}).catch((response) => {
-	    console.log(response);
+	    store.dispatch({
+			type: ActionTypes.ERROR_LOADING_DATA,
+			payload: response
+		});
 	});
 };
 
 
 const loadPersonalProjectsData = (store) => {
-	axios.get(LOAD_PERSONAL_PROJECTS_DATA_URL).then((response)=>{
+	axios.get(URLS.LOAD_PERSONAL_PROJECTS_DATA).then((response)=>{
 		store.dispatch({
 			type: ActionTypes.LOAD_PERSONAL_PROJECTS_DATA,
 			payload: response.data
 		});
 	}).catch((response) => {
-	    console.log(response);
+	    store.dispatch({
+			type: ActionTypes.ERROR_LOADING_DATA,
+			payload: response
+		});
 	});
 };
 
 
 const loadAboutData = (store) => {
-	axios.get(LOAD_ABOUT_DATA_URL).then((response)=>{
-		console.log(response)
+	axios.get(URLS.LOAD_ABOUT_DATA).then((response)=>{
 		store.dispatch({
 			type: ActionTypes.LOAD_ABOUT_DATA,
 			payload: response.data.description
 		});
 	}).catch((response) => {
-	    console.log(response);
+	    store.dispatch({
+			type: ActionTypes.ERROR_LOADING_DATA,
+			payload: response
+		});
 	});
 };
 
