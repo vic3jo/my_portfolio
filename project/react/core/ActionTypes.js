884d5d23dba95c98c0b11e7da4d54a9001c27a2e
Victor <vet6013@rit.edu>
2016-07-24 12:11:00 -0400

    Making constants files immutable

--- project/react/core/ActionTypes.js
+++ project/react/core/ActionTypes.js
@@ -3,7 +3,7 @@
  * Description: This file contains all the Action types constants that can
  *              be dispatched.
  */
-export default {
+export default Object.freeze({
   LOAD_EDUCATION_HISTORY_DATA: 'LOAD_EDUCATION_HISTORY_DATA',
   LOAD_EXPERIENCE_HISTORY_DATA: 'LOAD_EXPERIENCE_HISTORY_DATA',
   LOAD_ACADEMIC_PROJECTS_DATA: 'LOAD_ACADEMIC_PROJECTS_DATA',
@@ -14,4 +14,4 @@ export default {
   LOAD_PROFILE_PICTURE_DATA: 'LOAD_PROFILE_PICTURE_DATA',
   LOAD_SKILLS_DATA: 'LOAD_SKILLS_DATA',
   ERROR_LOADING_DATA: 'ERROR_LOADING_DATA',
-};
+});
